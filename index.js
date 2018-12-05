86150dc69b414beb377d912bdcaa2fd9389e56b3
Teddy Bradford <teddy@bearform.xyz>
2018-12-05 17:35:43 -0500

    Wrap components with ThemeProvider (requires a theme variable)

--- index.js
+++ index.js
@@ -49,6 +49,7 @@
 var React = require("react");
 var ReactDOMServer = require("react-dom/server");
 var components = {};
+var theme = {};
 // Literals
 var STR_WITH = "with";
 var STR_OBJ_OPEN = "{";
@@ -179,8 +180,13 @@ function parse(str, line, parser, types, stack, opts) {
 function renderReactComponentToString(componentName, props) {
   if (components.hasOwnProperty(componentName)) {
     var Component = components[componentName];
-    var reactElement = React.createElement(Component, props);
-    return ReactDOMServer.renderToString(reactElement);
+    var componentElement = React.createElement(Component, props);
+    var themeProviderElement = React.createElement(
+      components.ThemeProvider,
+      { theme },
+      componentElement
+    );
+    return ReactDOMServer.renderToString(themeProviderElement);
   } else {
     console.error(`"${componentName}" component does not exist in components supplied.
       Try extending @risd/webhook-react-tag with an object that includes ${componentName}.
@@ -196,6 +202,7 @@ module.exports = {
   parse: parse,
   extension: renderReactComponentToString,
   components: extendComponents,
+  theme: extendTheme,
   useTag: useTag
 };
 
@@ -216,6 +223,26 @@ function extendComponents() {
       throw new Error("`extendComponents` expects objects to be passed in.");
     }
     Object.assign(components, componentObject);
+  }
+  return this;
+}
+
+/**
+ * Helper to extend the possible theme that the react
+ * tag has access to rendering.
+ *
+ * Expects the arguments to be objects that contains keys
+ * that represent theme props.
+ *
+ * @return {object} this  Returns the current object context;
+ */
+function extendTheme() {
+  for (argumentIndex in arguments) {
+    var themeObject = arguments[argumentIndex];
+    if (typeof themeObject !== "object") {
+      throw new Error("`extendTheme` expects objects to be passed in.");
+    }
+    Object.assign(theme, themeObject);
   }
   return this;
 }
